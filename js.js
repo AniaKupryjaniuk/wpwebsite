862b8de5eb433561b72c8ed62dca56e44f95a129
AniaKupryjaniuk <57212584+AniaKupryjaniuk@users.noreply.github.com>
2020-04-03 10:27:25 +0200

    fetch data

--- js.js
+++ js.js
@@ -1,17 +1,21 @@
-//fetch("http://kuprdesign.com/t8/wp-json/wp/v2/posts?_embed")
-//  .then(res=>res.json())
-//.then(loopThroughPosts)
-//
-//function loopThroughPosts(posts){
-//  posts.forEach(post=>{
-//    const template = document.querySelector("template").content;
-//    const copy = template.cloneNode(true);
-//      console.log(posts)
-//    copy.querySelector("h2").textContent = post.title.rendered;
-//    copy.querySelector(".body").innerHTML=post.content.rendered;
-//    document.querySelector("main").appendChild(copy)
-//  })
-//
-//}
+fetch("http://kuprdesign.com/t8/wp-json/wp/v2/posts")
+    .then(res => res.json())
+    .then(handleData)
 
+function handleData(posts) {
+    posts.forEach(showPost)
+}
 
+function showPost(post) {
+    console.log(post)
+    const template =
+        document.querySelector("template").content;
+
+    const copy = template.cloneNode(true);
+
+    copy.querySelector("h2").textContent = post.title.rendered;
+    copy.querySelector(".body").innerHTML = post.content.rendered;
+
+    document.querySelector("main").appendChild(copy);
+
+}
