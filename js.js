4f1cfbc473858fc38c5b0e40838d93724c74b41d
AniaKupryjaniuk <57212584+AniaKupryjaniuk@users.noreply.github.com>
2020-04-03 10:09:08 +0200

    origin

---
+++ js.js
@@ -0,0 +1,17 @@
+//fetch("http://kuprdesign.com/t8/wp-json/wp/v2/posts?_embed")
+//  .then(res=>res.json())
+//.then(loopThroughPosts)
+//
+//function loopThroughPosts(posts){
+//  posts.forEach(post=>{
+//    const template = document.querySelector("template").content;
+//    const copy = template.cloneNode(true);
+//      console.log(posts)
+//    copy.querySelector("h2").textContent = post.title.rendered;
+//    copy.querySelector(".body").innerHTML=post.content.rendered;
+//    document.querySelector("main").appendChild(copy)
+//  })
+//
+//}
+
+
